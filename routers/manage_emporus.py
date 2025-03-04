6fde97a2731aced5d84ed5ef81897ec81f91e169
komposterius <komposter@gmail.com>
2025-03-04 08:58:21 +0100

    load_config draft

--- routers/manage_emporus.py
+++ routers/manage_emporus.py
@@ -1,9 +1,10 @@
 import logging
 import math
 import os
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 import pandas as pd
+import yaml
 from fastapi import APIRouter
 from sqlalchemy import create_engine
 
@@ -66,3 +67,37 @@ emporus_manager = EmporusTradeManager()
 @router.get("/emporus-trades", response_model=Dict[str, Any])
 async def emporus_trades(start_time: int = None, end_time: int = None):
     return emporus_manager.get_trades(start_time, end_time)
+
+
+@router.get("/get-data-collector-config/{container_name}", response_model=dict)
+async def get_config(container_name: str):
+    # Form the instance directory path correctly
+    instance_dir = os.path.join('bots', 'instances', container_name)
+
+    # reading the instance_id from the conf/conf_client.yml
+    conf_client_path = os.path.join(instance_dir, 'conf', 'conf_client.yml')
+    with open(conf_client_path, 'r') as conf_client:
+        data = yaml.safe_load(conf_client)
+        instance_id = data.get('instance_id', None)
+    if not instance_id:
+        return {"error": "Instance ID not found in conf_client.yml"}
+
+    # reading the controllers_config from the script config (conf/scripts/[instance_id].yml)
+    instance_id = instance_id.replace("hummingbot-", "")
+    script_config_path = os.path.join(instance_dir, 'conf', 'scripts', f"{instance_id}.yml")
+    with open(script_config_path, 'r') as script_config:
+        script_config = yaml.safe_load(script_config)
+        controllers_config = script_config.get('controllers_config', None)
+    if not controllers_config:
+        return {"error": "controllers_config not found in script config"}
+
+    # reading all the data collector configs from the controllers_config (conf/controllers/[controller_name].yml)
+    controller_configs = []
+    for controller_name in controllers_config:
+        controller_config_path = os.path.join(instance_dir, 'conf', 'controllers', f"{controller_name}")
+        with open(controller_config_path, 'r') as controller_config:
+            data = yaml.safe_load(controller_config)
+            controller_configs.append(data)
+
+    # return the script config and data collector configs
+    return {"script_config": script_config, "controller_configs": controller_configs}
